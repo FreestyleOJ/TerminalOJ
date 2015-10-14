4a070a9d2657a5a5f1a5055f4c59ab33c5c9607f
SCaffrey <imcaffrey@126.com>
2015-10-14 06:11:37 +0000

    update

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 "use strict";
-// var argv = require('yargs').argv;
 var color = require('bash-color');
-var path = require('path');
-var fs = require('fs');
 var _ = require('lodash');
 var program = require('commander');
-
-var submit = require('./submit');
+var submit = require('./lib/submit');
 
 program
     .version('0.0.2')
-    .usage('check more at https://github.com/freestyleoj/terminaloj')
+    .usage('check more at https://github.com/freestyleoj/terminaloj');
 program
     .command('OnlineJudge <cmd>')
     .alias('OJ')
     .description('specifying an OnlineJudge')
-    .option("-s, --set <aliasOfOJ>", "set an alias for an OnlineJudge")
-    .option("-l, --list", "list the OnlineJudges with the aliases")
+    .option('-s, --set <aliasOfOJ>', 'set an alias for an OnlineJudge')
+    .option('-l, --list', 'list the OnlineJudges with the aliases')
     .action(function(cmd, options){
-        var md = typeof options.set == 'string' ? options.set : "";
+        var md = typeof options.set == 'string' ? options.set : '';
         console.log('%s %s', cmd, md);
     }).on('--help', function() {
         console.log('  Examples:');
-        console.log('    terminaloj -l');
-        console.log('    terminaloj -s PKUOJ POJ');
+        console.log('    terminaloj OJ -l');
+        console.log('    terminaloj OJ -s PKUOJ POJ');
+        console.log();
+    });
+program
+    .command('submit <file>')
+    .alias('submit')
+    .description('submit a file')
+    .option('-f --file <name>', 'select a file to submit')
+    .action(function(cmd, options){
+        var fl = typeof options.file == 'string' ? options.file : '';
+        console.log(fl);
+        console.log(submit.getContent(fl));
+    }).on('--help', function(){
+        console.log('  Examples:');
+        console.log('    terminaloj submit -f a.cpp');
         console.log();
     });
 
 program.parse(process.argv);
-var ss = submit.getContent('app.js');
-if (ss.status == 'YES'){
+
+var fileContent = submit.getContent(__dirname + '/index.js');
+if (fileContent.status == 'YES'){
     console.log(color.green('File read successfully!'));
-    console.log(JSON.stringify(ss.content));
+    //console.log(JSON.stringify(fileContent.content));
+    //console.log(fileContent.content);
 }
